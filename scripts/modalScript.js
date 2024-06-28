3ddc22f0ad988df3bc2b83fcc628a3ccf94de40d
Aditya <adityakrsingh2604@gmail.com>
2024-06-28 13:08:40 +0530

    notepad and gallery done

---
+++ scripts/modalScript.js
@@ -0,0 +1,50 @@
+// ----------------- NOTEPAD MODAL -----------------
+let notepadIcon = document.getElementById('notepadIcon');
+let notepadModal = document.getElementById('notepadModal');
+let closeModal = document.getElementById('close');
+
+notepadIcon.addEventListener("click", (event) => {
+    console.log("notepadIcon button clicked");
+    event.stopPropagation();
+    if (notepadModal.style.display === "none" || notepadModal.style.display === "") {
+        notepadModal.style.display = "block";
+    } else {
+        notepadModal.style.display = "none";
+    }
+});
+
+closeModal.addEventListener("click", () => {
+    notepadModal.style.display = "none";
+});
+
+document.addEventListener("click", (event) => {
+    if (notepadModal.style.display === "block" && !notepadModal.contains(event.target) && event.target !== notepadIcon) {
+        notepadModal.style.display = "none";
+    }
+});
+
+
+// ----------------- GALLERY MODAL -----------------
+let galleryIcon = document.getElementById('galleryIcon');
+let galleryModal = document.getElementById('galleryModal');
+let galleryClose = document.getElementById('galleryClose');
+
+galleryIcon.addEventListener("click", (event) => {
+    console.log("galleryIcon button clicked");
+    event.stopPropagation();
+    if (galleryModal.style.display === "none" || galleryModal.style.display === "") {
+        galleryModal.style.display = "block";
+    } else {
+        galleryModal.style.display = "none";
+    }
+});
+
+galleryClose.addEventListener("click", () => {
+    galleryModal.style.display = "none";
+});
+
+document.addEventListener("click", (event) => {
+    if (galleryModal.style.display === "block" && !galleryModal.contains(event.target) && event.target !== galleryIcon) {
+        galleryModal.style.display = "none";
+    }
+});
