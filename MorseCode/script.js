290bdcb54100c63f9b4d73b635da46c4e5ba504e
Aditya <adityakrsingh2604@gmail.com>
2024-07-06 23:52:03 +0530

    comit

--- MorseCode/script.js
+++ MorseCode/script.js
@@ -1,42 +1,4 @@
-const morseCodeMap = {
-    "A": ".-", "B": "-...", "C": "-.-.", "D": "-..", "E": ".", "F": "..-.",
-    "G": "--.", "H": "....", "I": "..", "J": ".---", "K": "-.-", "L": ".-..",
-    "M": "--", "N": "-.", "O": "---", "P": ".--.", "Q": "--.-", "R": ".-.",
-    "S": "...", "T": "-", "U": "..-", "V": "...-", "W": ".--", "X": "-..-",
-    "Y": "-.--", "Z": "--.."
-};
-
-const morseToTextMap = {
-    ".-": "A", "-...": "B", "-.-.": "C", "-..": "D", ".": "E", "..-.": "F",
-    "--.": "G", "....": "H", "..": "I", ".---": "J", "-.-": "K", ".-..": "L",
-    "--": "M", "-.": "N", "---": "O", ".--.": "P", "--.-": "Q", ".-.": "R",
-    "...": "S", "-": "T", "..-": "U", "...-": "V", ".--": "W", "-..-": "X",
-    "-.--": "Y", "--..": "Z"
-};
-
-function translateTextToMorse(text) {
-    const morseCodeArray = [];
-    for (let char of text.toUpperCase()) {
-        if (!morseCodeMap[char]) {
-            throw new Error(`Invalid character: ${char}`);
-        }
-        morseCodeArray.push(morseCodeMap[char]);
-    }
-    return morseCodeArray.join(' ');
-}
-
-function translateMorseToText(morse) {
-    const morseCodes = morse.trim().split(' ');
-    const textArray = [];
-    for (let code of morseCodes) {
-        if (!morseToTextMap[code] && code !== '') {
-            throw new Error(`Invalid Morse code: ${code}`);
-        }
-        textArray.push(morseToTextMap[code]);
-    }
-    return textArray.join('');
-}
-
+import { translateTextToMorse, translateMorseToText } from './translator.js';
 
 const textToMorse = document.getElementById('textToMorse');
 const morseToText = document.getElementById('morseToText');
--- MorseCode/translator.js
+++ MorseCode/translator.js
@@ -1,40 +1,78 @@
 const morseCodeMap = {
-    "A": ".-", "B": "-...", "C": "-.-.", "D": "-..", "E": ".", "F": "..-.",
-    "G": "--.", "H": "....", "I": "..", "J": ".---", "K": "-.-", "L": ".-..",
-    "M": "--", "N": "-.", "O": "---", "P": ".--.", "Q": "--.-", "R": ".-.",
-    "S": "...", "T": "-", "U": "..-", "V": "...-", "W": ".--", "X": "-..-",
-    "Y": "-.--", "Z": "--.."
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--.."
 };
 
-const morseToTextMap = {
-    ".-": "A", "-...": "B", "-.-.": "C", "-..": "D", ".": "E", "..-.": "F",
-    "--.": "G", "....": "H", "..": "I", ".---": "J", "-.-": "K", ".-..": "L",
-    "--": "M", "-.": "N", "---": "O", ".--.": "P", "--.-": "Q", ".-.": "R",
-    "...": "S", "-": "T", "..-": "U", "...-": "V", ".--": "W", "-..-": "X",
-    "-.--": "Y", "--..": "Z"
-};
+function getKeyByValue(object, value) {
+    return Object
+        .keys(object)
+        .find(key => object[key] === value);
+}
 
 function translateTextToMorse(text) {
-    const morseCodeArray = [];
+    let finalMorse = "";
     for (let char of text.toUpperCase()) {
-        if (!morseCodeMap[char]) {
+        if (char === ' ') {
+            finalMorse += '  ';
+            continue;
+        }
+        const morse = morseCodeMap[char];
+        if (!morse) {
             throw new Error(`Invalid character: ${char}`);
         }
-        morseCodeArray.push(morseCodeMap[char]);
+        finalMorse += morse + ' ';
     }
-    return morseCodeArray.join(' ');
+    return finalMorse.trim();
 }
 
 function translateMorseToText(morse) {
     const morseCodes = morse.trim().split(' ');
     const textArray = [];
+    let spaceCount = 0;
     for (let code of morseCodes) {
-        if (!morseToTextMap[code] && code !== '') {
+        if (code === '') {
+            spaceCount++;
+            if (spaceCount === 2) {
+                textArray.push(' ');
+                spaceCount = 0;
+            }
+            continue;
+        }
+        const char = getKeyByValue(morseCodeMap, code);
+        if (!char) {
             throw new Error(`Invalid Morse code: ${code}`);
         }
-        textArray.push(morseToTextMap[code]);
+        textArray.push(char);
+        spaceCount = 0;
     }
     return textArray.join('');
 }
 
-module.exports = { translateTextToMorse, translateMorseToText };
+export {
+    translateTextToMorse,
+    translateMorseToText
+};
